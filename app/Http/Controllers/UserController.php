69939f6644228474c23b6ee4fb4ff596dd548720
Nikita Kozlov <nikita@thetourguy.com>
2023-01-21 13:38:50 +0300

    .

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,10 +8,10 @@ use Illuminate\Http\Request;
 class UserController extends Controller
 {
     public function Index(){
-        return User::all();
+        return User::all()->makeHidden('email_verified_at');
     }
 
     public function Show(User $user){
-           return $user;
+        return $user->makeHidden('email_verified_at');
     }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'birth_date'
     ];
 
     /**
@@ -40,5 +41,6 @@ class User extends Authenticatable
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
+        'birth_date' => 'date',
     ];
 }
